e91bc770a52898527ade242d28218343beea1314
silassequeira <uc2021220102@student.uc.pt>
2024-11-18 20:46:28 +0000

    here is the config file, try to make it work with your code !

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$str = "dbname=postgres user=postgres password=postgres host=localhost 
+port=5432";
+
+$connection = pg_connect($str);
+
+if (!$connection) {
+    die("Erro na ligacao");
+    }
+    echo "Ligacao estabelecida!";
+    pg_close($connection);
+?>
